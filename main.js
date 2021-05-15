fd4b2944f998946efb39db0d992571fc84fa53af
Superben356 <75051639+Superben356@users.noreply.github.com>
2021-05-14 20:01:35 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+Webcam.set({
+    width:310,
+    height:300,
+    image_format:'png',
+    png_quality:90,
+
+
+    constraints:{
+        facingMode:"Environment"
+    }
+
+});
+camera=document.getElementById("camera");
+
+Webcam.attach('#camera');
